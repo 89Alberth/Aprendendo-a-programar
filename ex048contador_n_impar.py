e3c0e4bd79bce375f9c9a4b06585eca93addf8ff
Alberth R C santana <82107666+89Alberth@users.noreply.github.com>
2021-11-02 16:11:35 -0300

    exercicio

--- ex048contador_n_impar.py
+++ ex048contador_n_impar.py
@@ -4,4 +4,4 @@ for n in range(1, 501, 2):
     if n % 3 == 0:
         c = c + 1
         soma = soma + n
-print('A soma entre todos os {} numeros impar de 1 à 500 divivisivel por 3 é {}'.format(c, soma))
+print('A soma entre todos os {} numeros impar de 1 à 500 divisivel por 3 é {}'.format(c, soma))
